89ca1330343c1d5ae1283fa10955148b3ff5d7bc
Shakiv Hussain <51313301+shakivhussain@users.noreply.github.com>
2023-04-28 19:37:52 +0530

    Basic Implementation

--- app/src/main/java/com/shakiv/husain/firstcompose/MainActivity.kt
+++ app/src/main/java/com/shakiv/husain/firstcompose/MainActivity.kt
@@ -3,43 +3,27 @@ package com.shakiv.husain.firstcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.shakiv.husain.firstcompose.ui.theme.FirstComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FirstComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Text(text = "Hello Shakiv Husain")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun sayShakivHusain(name: String) {
+    Text(text = "Say : $name")
 }
 
-@Preview(showBackground = true)
+
+@Preview(showBackground = true, name = "Shakib1", showSystemUi = true, widthDp = 400, heightDp = 400)
 @Composable
-fun GreetingPreview() {
-    FirstComposeTheme {
-        Greeting("Android")
-    }
+fun previewFunction() {
+    sayShakivHusain(name = "Shakib Mansoori")
 }
