6292da6538ead79d918c46a15cb7c25f62f49531
Shakiv Hussain <51313301+shakivhussain@users.noreply.github.com>
2023-04-28 22:40:45 +0530

    Text View

--- app/src/main/java/com/shakiv/husain/firstcompose/MainActivity.kt
+++ app/src/main/java/com/shakiv/husain/firstcompose/MainActivity.kt
@@ -5,7 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,12 +24,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun sayShakivHusain(name: String) {
-    Text(text = "Say : $name")
+    Text(
+        text = "Say : $name",
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.ExtraBold,
+        color = Color.Blue,
+        fontSize = 36.sp,
+        textAlign = TextAlign.Center
+    )
 }
 
 
 @Preview(showBackground = true, name = "Shakib1", showSystemUi = true, widthDp = 400, heightDp = 400)
 @Composable
 fun previewFunction() {
-    sayShakivHusain(name = "Shakib Mansoori")
+//    sayShakivHusain(name = "Shakib Mansoori")
+
 }
