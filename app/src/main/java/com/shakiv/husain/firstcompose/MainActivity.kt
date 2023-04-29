3e5995019a4dde8a74afaa9a5465047539f6c33e
Shakiv Hussain <51313301+shakivhussain@users.noreply.github.com>
2023-04-29 12:51:10 +0530

    ✅ Modifier

--- app/src/main/java/com/shakiv/husain/firstcompose/MainActivity.kt
+++ app/src/main/java/com/shakiv/husain/firstcompose/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,7 +24,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
@@ -34,7 +41,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            textFieldComposable()
+            ImageRound()
         }
     }
 }
@@ -44,25 +51,72 @@ class MainActivity : ComponentActivity() {
     showBackground = true,
     name = "Shakiv Husain",
     showSystemUi = true,
+    widthDp = 300,
+    heightDp = 300
 )
 @Composable
 fun previewFunction() {
-
-
-    Column(verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.Start) {
-        ListViewItem(imageId = R.drawable.my_img, title = "Shakiv Husain", designation = "Android Developer")
-        ListViewItem(imageId = R.drawable.my_img, title = "Shakiv Husain", designation = "Android Developer")
-        ListViewItem(imageId = R.drawable.my_img, title = "Shakiv Husain", designation = "Android Developer")
-        ListViewItem(imageId = R.drawable.my_img, title = "Shakiv Husain", designation = "Android Developer")
-
-    }
-
+    ImageRound()
 }
 
 
 @Composable
-private fun ListViewItem(imageId: Int, title: String, designation: String) {
-    Row(Modifier.padding(16.dp)) {
+private fun ImageRound() {
+    Image(
+        painter = painterResource(id = R.drawable.my_img),
+        contentDescription = "My Image", contentScale = ContentScale.Crop,
+        modifier =
+        Modifier.size(500.dp).clip(CircleShape)
+            .border(2.dp, Color.Green, CircleShape)
+            .shadow(100.dp, RectangleShape,true,Color.Blue)
+
+    )
+}
+
+@Composable
+private fun ModifierConcept() {
+    Text(
+        text = "Shakiv Husain",
+        color = Color.White,
+        modifier = Modifier
+            .background(Color.Blue)
+            .size(100.dp)
+            .padding(56.dp)
+            .border(4.dp, Color.White)
+            .clip(CircleShape)
+            .background(Color.White)
+            .clickable {
+
+            },
+    )
+}
+
+@Composable
+private fun ListOfItems() {
+    Column(verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.Start) {
+        ListViewItem(
+            imageId = R.drawable.my_img, title = "Shakiv Husain", designation = "Android Developer",
+        )
+        ListViewItem(
+            imageId = R.drawable.my_img, title = "Shakiv Husain", designation = "Android Developer"
+        )
+        ListViewItem(
+            imageId = R.drawable.my_img, title = "Shakiv Husain", designation = "Android Developer"
+        )
+        ListViewItem(
+            imageId = R.drawable.my_img, title = "Shakiv Husain", designation = "Android Developer"
+        )
+
+    }
+}
+
+@Composable
+private fun ListViewItem(
+    imageId: Int, title: String, designation: String, modifier: Modifier = Modifier.padding(16.dp)
+) {
+
+    // called chaining
+    Row(modifier.padding(16.dp)) {
         Image(
             painter = painterResource(id = imageId),
             contentDescription = "My Image",
